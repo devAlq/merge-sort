dbbdfe1bd512107019a080ed4872e214eb14b375
devAlq <khalidAlq6@gmail.com>
2024-06-29 17:20:21 +0300

    taskDone

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,65 @@ public class Main {
 
   public static void main(String[] args){
 
-  /* your code here */
+  int[] array = {
+    66,22,33,11,44
+  };
+
+int [] mergedArray = merge(array);
+for(int x = 0; x < mergedArray.length; x++){
+  System.out.println(mergedArray[x]);
+}
+
+
+  
 	
   }
-}
+
+  public static int[] merge(int[] array){
+    if(array.length <= 1){
+      return array;}
+    int mid = array.length/2;
+
+    int [] left = new int[mid];
+    int[] right = new int[array.length - mid];
+    System.arraycopy(array, 0, left, 0, mid);
+    System.arraycopy(array, mid, right, 0, array.length - mid);
+
+
+    left = merge(left);
+
+    right = merge(right);
+
+
+
+    return sort(left, right);
+    
+     }
+  public static int[] sort(int[] left, int[] right){
+    int[] mergedArray = new int[left.length + right.length];
+    int i = 0;
+
+    int j = 0;
+    int k = 0;
+    while(i < left.length && j< right.length){
+      if(left[i] <= right[j]){
+        mergedArray[k] =left[i];
+        i++;
+    }else{
+      mergedArray[k] = right[j];
+      j++;
+    }k++;
+
+
+    
+  }
+  while (i < left.length) {
+    mergedArray[k] = left[i];
+    i++;
+    k++;
+  }while (j < right.length) {
+    mergedArray[k] = right[j];
+    j++;
+    k++; 
+  }  return mergedArray;
+}}
